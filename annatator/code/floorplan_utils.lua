8616de6fca61f15103d6003d70c77e83525b56d3
rgresia <45833466+rgresia@users.noreply.github.com>
2021-04-27 21:12:48 -0400

    fixes for wall detection

--- annatator/code/floorplan_utils.lua
+++ annatator/code/floorplan_utils.lua
@@ -1,9 +1,9 @@
 require 'csvigo'
 require 'image'
 local pl = require 'pl.import_into' ()
---cv = require 'cv'
---require 'cv.imgproc'
---py = require('python')
+cv = require 'cv'
+require 'cv.imgproc'
+py = require('python')
 --paths.dofile('/home/chenliu/Projects/Floorplan/floorplan/InverseCAD/models/SpatialSymmetricPadding.lua')
 
 local utils = {}
